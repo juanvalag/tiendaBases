5df8ff4797eb993717c650583e9662850258dd1b
juanvalag <juanvalag@unisabana.edu.co>
2019-10-24 11:42:06 -0500

    se añadió el prueba.java

---
+++ tiendaBases/src/java/model/Prueba.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+
+public class Prueba {
+
+private String a,b,c,d,e;
+
+public Prueba(){
+this.a="";
+this.b="";
+this.c="";
+this.d="";
+this.e="";
+}
+
+public Prueba(String a,String b,String c,String d,String e){
+this.a=a;
+this.b=b;
+this.c=c;
+this.d=d;
+this.e=e;
+}
+
+
+}
