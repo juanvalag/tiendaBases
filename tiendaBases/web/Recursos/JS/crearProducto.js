e814346a9843e3d30bc7a96262bd067327e419f8
juanvalag <juanvalag@unisabana.edu.co>
2019-11-15 14:43:11 -0500

    se añadio´el gestionde producto del proveedor para el crea producto

--- tiendaBases/web/Recursos/JS/crearProducto.js
+++ tiendaBases/web/Recursos/JS/crearProducto.js
@@ -4,23 +4,36 @@ var proveedores;
 const init = () => {
     proveedores = document.getElementsByClassName("proves");
 };
-const anadirProveedor = (event) => {
-    alert("hola");
+const anadirProveedor = () => {
+    let selector = document.getElementById("proveedores");
+    let seleccionado = selector.options[selector.selectedIndex];
     let fila = document.createElement("tr");
     let idaAnadir = document.createElement("td");
     let nombreaAnadir = document.createElement("td");
-    let id = event.target.id;
-    let nombre = event.target.innerHTML;
+    let hiddenaAnadir = document.createElement("input");
+    let inputPrecioAnadir = document.createElement("input");
+    hiddenaAnadir.type = "hidden";
+    hiddenaAnadir.name = "proveedores";
+    let id = seleccionado.id;
+    let nombre = seleccionado.innerHTML;
     for (let temp of proveedores) {
         if (temp.id === id && temp.innerHTML === nombre) {
+            hiddenaAnadir.value = temp.id;
             idaAnadir.innerHTML = temp.id;
             nombreaAnadir.innerHTML = temp.innerHTML;
+            inputPrecioAnadir.type = "number";
+            inputPrecioAnadir.min = 1;
+            inputPrecioAnadir.value = 1;
+            inputPrecioAnadir.name = temp.id;
+            inputPrecioAnadir.required = true;
+            selector.removeChild(seleccionado);
+            fila.appendChild(idaAnadir);
+            fila.appendChild(nombreaAnadir);
+            fila.appendChild(inputPrecioAnadir);
+            document.getElementById("provesSeleccionados").appendChild(hiddenaAnadir);
+            document.getElementById("tbody").appendChild(fila);
         }
     }
-    event.target.parentNode.removeChild(event.target);
-    fila.appendChild(idaAnadir);
-    fila.appendChild(nombreaAnadir);
-    document.getElementById("tbody").appendChild(fila);
-    alert("hola");
+
 };
 window.onload = init;
